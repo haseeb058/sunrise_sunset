04b2e82c083768dae7e85d4659f59ec6fee04433
haseeb058 <meharhaseeb058.com>
2023-02-10 22:49:44 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [location, setLocation] = useState({});
+  const [sunData, setSunData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.ipbase.com/v2/info?apikey=zDDsQgZGUHsFywanM9pqnQ10XlibR538NHt16YCe"
+      )
+      .then((res) => {
+        setLocation(res?.data?.data?.location);
+        getSunRiseSunSet(res);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  const getSunRiseSunSet = (res) => {
+    axios
+      .get(
+        `https://api.sunrise-sunset.org/json?lat=${res?.data?.data?.location?.latitude}&lng=${res?.data?.data?.location?.longitude}`
+      )
+      .then((res) => {
+        setSunData(res?.data?.results);
+        console.log(res);
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sunrise and Sunset in Your Area</h1>
+      <p>Latitude: {location.latitude}</p>
+      <p>Longitude: {location.longitude}</p>
+      <p>Sunrise: {sunData?.sunrise}</p>
+      <p>Sunset: {sunData?.sunset}</p>
     </div>
   );
-}
+};
 
 export default App;
